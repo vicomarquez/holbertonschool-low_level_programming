65ccda0495c5ac8b510d96c83790eaa16bb3262f
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-09 10:14:35 -0700

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch ++) 
+	{
+		_putchar(ch);
+	}
+	_putchar('\n')
+}
