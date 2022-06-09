e441bb4410f0d142ff9f32cf10f8b9ebf4b24365
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-09 10:43:52 -0700

    task 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
