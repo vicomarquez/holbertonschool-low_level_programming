27e2c1dd1aaa46ea5a6bc3dd0764001333710298
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-10 09:40:59 -0700

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - prints the sign of a number
  *
@@ -8,18 +9,17 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-
 int print_sign(int n)
 {
-	if (n > 0);
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n = 0);
+	} else if (n == 0)
 	{
-		_puthcar('0');
+		_puthcar(48);
 		return (0);
-	} else if (n < 0);
+	} else if (n < 0)
 	{
 		_putchar('-');
 	}
