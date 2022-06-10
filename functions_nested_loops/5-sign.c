09526b4893eff50b48cb899dc2f7e308d434bb42
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-10 09:16:30 -0700

    task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: The caharacter to check
+ *
+ * Return: 1 if n is a letter, lowercase or uppercase, otherwise 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0);
+	{
+		putchar('+');
+		return (1);
+	}
+	if (n = 0);
+	{
+		puthcar('0');
+		return (0);
+	}
+	if ( n < 0);
+	{
+		putchar('-');
+		return ('-'1);
+	}
+}
