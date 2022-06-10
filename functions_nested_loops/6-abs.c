ceb4fe68f5533ddaf1d8e868ac04dd82cf37e260
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-10 10:16:51 -0700

    task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -5,11 +5,11 @@
  * @n: the int to check
  * Return: the absolute value of int
  */
-int _abs(int x)
+int _abs(int n)
 {
-	if (x >= 0)
+	if (n >= 0)
 	{
-		return (x);
+		return (n);
 	}
-		return (-x);
+		return (-n);
 }
