4b4fa4e49519e934e0a0ff5bf383370ed82e2234
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-10 07:26:11 -0700

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - checks for alphabetic character
+ *
+ * @c: The caharacter to check
+ *
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
