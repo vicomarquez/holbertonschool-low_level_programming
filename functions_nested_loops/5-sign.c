80494bf0e53bbd593e95bd6a5517883389458a0e
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-10 09:23:02 -0700

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,17 +9,18 @@
 
 int print_sign(int n)
 {
-	if (n > 0);
+	if(n > 0);
 	{
 		putchar('+');
 		return (1);
 	}
-	if (n = 0);
+	
+	else if(n = 0);
 	{
 		puthcar('0');
 		return (0);
 	}
-	if ( n < 0);
+	else if(n < 0);
 	{
 		putchar('-');
 		return ('-'1);
