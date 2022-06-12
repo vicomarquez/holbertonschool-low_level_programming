8e947c3347696123d6880279261fcf5444d33f4b
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 15:01:47 -0700

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_numbers - multiplies two integers
+ *_putchar - adds a new line
+ *@x: integer
+ *Return: always 0
+*/
+
+void print_numbers(void)
+{
+	int x;
+	for (x >= '0' ; x <= '9' ; x++)
+		_putchar('\n');
+}
