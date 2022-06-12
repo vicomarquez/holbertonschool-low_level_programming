dd658a81645d1ef88f50e187467f94153f533090
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 15:33:35 -0700

    task 5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,19 @@ void more_numbers(void)
 {
 	int x;
 	int y;
+	int z;
 
-	for (x = 1 ; x <= 10 ; x++)
+	for (x = 0 ; x <= 10 ; x++)
 	{
-		for (y = '0' ; y <= '14' ; y++)
+		for (y = 0 ; y <= 14 ; y++)
 	{
-		_putchar(x);
+		z = y + '0';
+		if (y > 9)
+		{
+			z = (y % 10) + '0';
+			_putchar('1');
+		}
+		_putchar(z);
 	}
 		_putchar('\n');
 	}
