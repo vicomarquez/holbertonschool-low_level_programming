39e875ed7bba925dcd5904293736cb34663c5354
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 06:24:29 -0700

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+ #include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * @a: hours
+ * @b: hours
+ * @c: minutes
+ * @d: minutes
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0 ; a <= 2 ; a++)
+	{
+		for (b = 0 ; b <= 9 ; b++)
+		{
+			if (a == 2 && b >= 4)
+				break;
+			for (c = 0 ; c < 6 ; c++)
+			{
+				for (d = 0 ; d <= 9 ; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(:);
+					_putchar(c);
+					_putchar(d);
+				}
+			}
+		}
+	}
+}
