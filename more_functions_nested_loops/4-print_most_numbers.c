255dd0a37bc82555b578aa062f4a945b99ced305
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 15:12:31 -0700

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_most_numbers - prints numbers from 0 to 9
+ *_putchar - adds a new line
+ *Return: always 0
+*/
+
+void print_most_numbers(void)
+{
+	int x;
+
+	while (x = '0' ; x <= '9' ; x++)
+	{
+		if (x != '2' && x != '4')
+		_putchar(x);
+	}
+		_putchar('\n');
+}
