2479163ac9c066126ba65ecf78c2014559403cc3
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 08:28:06 -0700

    readme

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
+ *_isupper - checks for uppercase character
  *
- *
- *
+ *Return: 1 if c is upercase, 0 otherwise
 */
 
 int _islower(int c)
 {
-	if (c >= 'A' ; c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
