a3939f538c2baf1738c91057615019f6bc496479
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 16:15:44 -0700

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_square - prints a square
+ *_putchar - adds a new line
+ *@n: chracter to check
+ *Return: always 0
+*/
+
+void print_square(int size)
+{
+	char a, b;
+
+	if (size <= 0)
+	{
+		size = 0;
+		_putchar('\n');
+	}
+	for (a = 0; a < size ; a++)
+	{
+		b = size;
+		for (; b > 0 ; b--)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
