bb9cb51136e69707a3ed5e72ecc2b63da2bdf117
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 06:44:14 -0700

    task 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+ #include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for a in range(0, 9):
+	{
+		for b in range(a, (i*9)+1, a):
+			print(b, end="\t")
+			print()
+	}
+}
