d1139e239d5fc4a5a053c27cfc381689b9922d06
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 06:29:35 -0700

    task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,15 +12,15 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0 ; a <= 2 ; a++)
+	for (a = '0' ; a <= '2' ; a++)
 	{
-		for (b = 0 ; b <= 9 ; b++)
+		for (b = '0' ; b <= '9' ; b++)
 		{
-			if (a == 2 && b >= 4)
+			if (a == '2' && b >= '4')
 				break;
-			for (c = 0 ; c < 6 ; c++)
+			for (c = '0' ; c < '6' ; c++)
 			{
-				for (d = 0 ; d <= 9 ; d++)
+				for (d = '0' ; d <= '9' ; d++)
 				{
 					_putchar(a);
 					_putchar(b);
