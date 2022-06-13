99ec8f032d7abc822dea1054a5758eaef529d9ca
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-12 17:36:18 -0700

    task 9

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_triangle - prints a triangle
+ *_putchar - adds a new line
+ *@size: value to check
+ *Return: always 0
+*/
+
+void print_triangle(int size)
+{
+	char a, b, c;
+
+	if (size <= 0)
+	{
+		size = 0;
+		_putchar('\n');
+	}
+	for (a = 0; a < size ; a++)
+	{
+		b = size;
+		c = a;
+		for (c = 0; c < b ; c++)
+		{
+			if ((a + c) >= (size - c))
+			_putchar('#');
+			else
+				_putchar(' ');
+	}
+		_putchar('\n')
+}
