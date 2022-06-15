a5419d7220e113d95ff9e0bf3b00afe4f56602eb
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-15 08:59:52 -0700

    task 1

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_strlen - returns the length of a string
+ *@a: character to check
+ *@b: character to check
+ */
+int _strlen(char *s)
+{
+	char length;
+
+	return ( (char *s[length]));
+}
