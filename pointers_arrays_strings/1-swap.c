78cd97e3510cc4642521b6143fd1bc43c88022e1
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-15 08:44:27 -0700

    task 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * void swap_int(int *a, int *b - takes a pointer to an int and updates the value to 98
- *@c: character to check
+ *swap_int - swaps two values
  *@a: character to check
  *@b: character to check
  */
 void swap_int(int *a, int *b)
 {
 	int c;
+
 	c  = *a;
 	*a = *b;
 	*b = c;
