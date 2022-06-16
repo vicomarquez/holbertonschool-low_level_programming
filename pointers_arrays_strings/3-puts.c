80e8bf7eefdc98643e84a0f56e38d55c808c7d64
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-16 08:06:27 -0700

    task 2

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - returns the length of a string
+ *@s: character to check
+ *Return: the length of a string
+ */
+void _puts(char *str)
+{
+	char *str = a;
+
+	_puts (str);
+	{
+	}
+	return (0);
+}
