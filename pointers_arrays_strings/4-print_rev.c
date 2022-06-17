294b3cdcaea883553dc90a35caff2f97ff608fe1
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-17 13:46:58 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
  * print_rev - prints a string in reverse
- * _strlen - length of a string
+ * _strlen - shows length of a string
  *@s: character to check
- *@a: character to check
  *Return: the length of a string
  */
 void print_rev(char *s)
@@ -25,6 +24,6 @@ int _strlen(char *s)
 	for (l = 0 ; s[l] != '\0' ; l++)
 	{
 	}
-	return(l);
+	return (l);
 }
 
