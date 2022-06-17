f8396bc9dcec3ca2f425a22f577ddf64a4460e6d
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-17 13:42:06 -0700

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,14 @@
  *@a: character to check
  *Return: the length of a string
  */
-void print_rev(char *a)
+void print_rev(char *s)
 {
-	int i, length;
+	int i;
+	int length = _strlen;
 
-	for (i = length - 1 ; i >= 0 ; i--);
+	for (i = length - 1 ; i >= 0 ; i--)
 	{
-		_putchar (a[i]);
+		_putchar (s[i]);
 	}
 	_putchar ('\n');
 }
