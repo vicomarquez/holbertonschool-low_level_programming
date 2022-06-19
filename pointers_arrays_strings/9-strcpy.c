6ee58b3dca1415faacc43728c8fde2fc97e1cfc9
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-18 19:02:13 -0700

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * *_strcpy - copies a string
+ *@dest: character to check
+ *@src: character to check
+ *Return: the length of a string
+ */
+void puts_half(char *dest, char *src)
+{
+	int a = 0;
+	int length = _strlen(str);
+
+	while (i < length)
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	return (dest);
+}
+/**
+ * _strlen - shows length of a string
+ * @s: character to check
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0 ; s[l] != '\0' ; l++)
+	{
+	}
+	return (l);
+}
+
