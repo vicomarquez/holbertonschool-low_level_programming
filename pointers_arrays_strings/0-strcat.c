37b64e7a1016c5315483c8b8e83e30350f1815d5
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-18 19:32:02 -0700

    task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * *_strcpy - copies a string
+ *@dest: character to check
+ *@src: character to check
+ *Return: the length of a string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
+/**
+ * _strlen - shows length of a string
+ * @s: character to check
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	for (l = 0 ; s[l] != '\0' ; l++)
+	{
+	}
+	return (l);
+}
+
