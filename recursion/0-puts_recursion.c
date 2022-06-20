44d9ef2bb472bf12a0967439f8fab743a72caa09
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-20 13:12:35 -0700

    task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_puts_recursion - 
+ *
+ *
+ *@s: string
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	int a;
+
+	if (a != '\0')
+	{
+		_putchar(s[a]);
+		a++;
+	}
+	return;
+}
