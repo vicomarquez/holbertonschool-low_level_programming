7db1572501645a9ed7a2a8c098aa541e00e7c468
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-20 15:39:52 -0700

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,13 +9,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_print_rev_recursion(s - 1);
+		_print_rev_recursion(s + 1);
 	}
 }
