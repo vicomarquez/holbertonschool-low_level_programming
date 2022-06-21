fca97c3c3c37d618be884e7b60cdce113a19a70e
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-21 08:27:08 -0700

    task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,11 +9,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int l = 0;
 
-	if (s[i] == '\0')
+	if (s[l] == '\0')
 	{
 		return (l);
-		_strlen_recursion(s + i++);
+		_strlen_recursion(s + l++);
 	}
 }
