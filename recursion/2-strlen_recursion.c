8f68ca6066223f84442b3cf5bd4579b42390c089
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-21 08:24:54 -0700

    task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,15 +2,18 @@
 /**
  *_strlen_recursion - returns the length of a string
  *
- *
  *@s: string
+ *Return: length of string
  *
  */
 
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	int i = 0;
+
+	if (s[i] == '\0')
 	{
+		return (l);
+		_strlen_recursion(s + i++);
 	}
-		return (*s);
 }
