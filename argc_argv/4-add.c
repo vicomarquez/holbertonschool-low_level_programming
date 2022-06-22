08156348267c553180c6ba1452c4f7692d3515a8
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-21 19:39:27 -0700

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all arguments
+ *
+ *@argc: counter
+ *
+ *@argv: arrays vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	long j;
+	char a = '\0';
+	char *b;
+	int c = 1
+
+	b = &a;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (c < argc)
+	{
+		c++;
+		j += strtol(argv[c], &b, 10);
+		if (*b != '\0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%ld\n", j);
+	return(0);
+}
