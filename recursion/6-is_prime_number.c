6e887c359039ad4ab3adb44293e11223073d1ab4
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-22 14:44:14 -0700

    task 6

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ *is_prime_number - returns 1 if the integer is a prime number
+ *
+ *@n: integer
+ *
+ *Return: something
+ *
+ */
+
+int is_prime_number(int n)
+{
+	int y;
+	int *p;
+
+	y = n;
+	p = &y;
+
+	if (y < 0)
+		y = -1;
+	else if (y == 0)
+		y = 0;
+	else
+		_rec(p, 2);
+	return (y);
+}
+/**
+ *_rec - recurses something
+ *
+ *@p: pointer
+ *
+ *@j: integer
+ *Return: something
+ */
+void _rec(int *p, int j)
+{
+	if (*p % j != 0 && 2 * j < n)
+	{
+		j++;
+		_rec(p, j);
+	}
+	else if (*p % j == 0)
+		*p = 0;
+	else
+		*p = 1;
+}
--- recursion/main.h
+++ recursion/main.h
@@ -6,3 +6,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 void _rec(int *p, int j);
+int is_prime_number(int n);
