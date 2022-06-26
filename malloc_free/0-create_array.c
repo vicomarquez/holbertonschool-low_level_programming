d4fa2bc6fd6a0a1363b4019e94ce17870d260f8d
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-26 08:28:35 -0700

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,7 +3,9 @@
 /**
  * *create_array - creates an array of chars.
  *
+ *@size: integer
  *
+ *@c: char
  *
  *Return: NULL
  */
@@ -13,19 +15,16 @@ char *create_array(unsigned int size, char c)
 	char *i;
 	unsigned int j;
 
-	j = 0;
-
-	if (size == 0)
+	if(size == 0)
 		return (NULL);
 
 	i = malloc (size * sizeof(char));
 
-	if (i == NULL)
+	if(i == NULL)
 		return (NULL);
 
-	while (j < size)
+	for (j = 0 ; j < size ; j++)
 	{
-		j++;
 		i[j] = c;
 	}
 	return (i);
