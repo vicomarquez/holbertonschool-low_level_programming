96d2c22a6e32566e9fbdc85d1020c756ffbae18b
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-26 19:11:42 -0700

    task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *_calloc - creates an array of integers
+ *
+ *@nmemb: integer
+ *
+ *@size: int
+ *
+ *Return: NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *i;
+	unsigned int j;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	i = malloc(nmemb * size);
+
+	if (i == NULL)
+		return (NULL)
+
+	for (j = 0 ; j < (nmemb * size) ; j++)
+		i[j] = 0;
+
+	return (i);
+}
