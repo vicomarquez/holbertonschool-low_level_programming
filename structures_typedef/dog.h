4cbd5f340d187a3924db9a78add508dd2b123237
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-28 09:26:12 -0700

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Defines a new type
+ *
+ *Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+int main(void)
+{
+	struct dog *Dog;
+
+	Dog->name = name;
+	Dog->owner = owner;
+	Dog->age = age;
+	return (0);
+}
