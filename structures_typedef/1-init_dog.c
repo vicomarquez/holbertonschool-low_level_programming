06de28ad413e0886838aca1e1afdd20683218061
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-28 10:38:43 -0700

    task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - Defines a new type
  *@name: dog's name
@@ -11,7 +11,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d != 0)
 	{
 		d->name = name;
 		d->age = age;
