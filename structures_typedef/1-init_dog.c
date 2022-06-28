f632f7797e8d00168008fbac61502406ae15eec8
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-28 10:23:26 -0700

    task 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * init_dog - Defines a new type
+ *@name: dog's name
+ *@owner: dog's owner
+ *@age:dog's age
+ *@d: struct
+ *Return: 0
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d = 0)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}		
+#endif
