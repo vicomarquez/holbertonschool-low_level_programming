ad16dac537c4ea1d917ca5a2cb3dc6ec82cce725
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-28 09:50:58 -0700

    task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DOG
+#define DOG
+#endif
 /**
  * struct dog - Defines a new type
- *
+ *@name: dog's name
+ *@owner: dog's owner
+ *@age:dog's age
  *Return: 0
  */
 
