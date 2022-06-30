5b729068f127fb28ad544f68190c12a280f8fcec
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-30 16:27:32 -0700

    task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,8 +10,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action))
 {
-	size_t i;
+	int i;
 
-	for(i = 0 ; i <= size ; i++)
+	if (array == NULL || action == NULL)
+		return;
+
+	for(i = 0 ; i < size ; i++)
 		(*action)(array[i]);
 }
