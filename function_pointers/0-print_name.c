8dd5ee734bab4b285a33b43ada0f4a17650fd666
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-29 20:04:34 -0700

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ *@name: pointer
+ *@f: pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
