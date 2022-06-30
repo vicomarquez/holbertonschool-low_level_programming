1d0ec9d311456d760b43e87c2f53ce974124428c
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-30 16:39:58 -0700

    task 1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,18 +1,18 @@
 #include "function_pointers.h"
-#include <stddef.h>
+#include <stdio.h>
 /**
  * array_iterator - executes a function
  *@array: pointer
  *@size: pointer
  *@action: pointer
  */
-void array_iterator(int *array, size_t size, void (*action))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
+	 unsigned int i;
 
 	if (array == NULL || action == NULL)
 		return;
 
 	for(i = 0 ; i < size ; i++)
-		action(array[i]);
+		(*action)(array[i]);
 }
