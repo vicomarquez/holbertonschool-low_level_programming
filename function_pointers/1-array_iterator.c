8d7c5407eafd4fc4cec60717fcc48c694d64416f
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-30 10:07:39 -0700

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes a function
+ *@array: pointer
+ *@size: pointer
+ *@action: pointer
+ */
+void array_iterator(int *array, size_t size, void (*action))
+{
+	int i;
+
+	for(i = 0 ; i = size ; i++)
+		(*action)(array[i]);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef FPOINT
 #define FPOINT
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)
 #endif
