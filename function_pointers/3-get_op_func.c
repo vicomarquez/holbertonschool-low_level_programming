990b121236a77b546f8247bb5b0a392224c4516f
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-30 19:37:04 -0700

    task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	   i = 0;
 	   while (ops[i].op)
 	   {
-		   if (*(ops[i].op) == *s || (s + 1) == '\0')
+		   if (*(ops[i].op) == *s && *(s + 1) == '\0')
 			   return (ops[i].f);
 		   i++;
 	   }
