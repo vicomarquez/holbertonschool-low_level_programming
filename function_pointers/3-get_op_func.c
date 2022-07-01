3429f5ed97b8c4405c37e2d82ea56cad182b83b3
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-30 19:16:45 -0700

    task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -19,9 +19,10 @@ int (*get_op_func(char *s))(int, int)
 	   };
 	   int i;
 
+	   i = 0;
 	   while (ops[i].op)
 	   {
-		   if (ops[i].op == s || s++ == NULL)
+		   if (*(ops[i].op) == *s || (s + 1) == '\0')
 			   return (ops[i].f);
 		   i++;
 	   }
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -7,7 +7,21 @@
  *@size: pointer
  *Return: 0
  */
-int main(
+int main(char **argv, int argc)
 {
-	 
+	int (*hi)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	hi = get_op_func(argv[2]);
+	if(!hi)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", hi(atoi(argv[1], atoi(argv[3])));
+	return (0);
 }
