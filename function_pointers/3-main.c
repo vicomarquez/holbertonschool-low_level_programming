0cecde7cebedcb0d9a092483889711733d93c257
vicomarquez <vicomarquezmuniz@gmail.com>
2022-06-30 19:26:33 -0700

    task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -7,21 +7,30 @@
  *@size: pointer
  *Return: 0
  */
-int main(char **argv, int argc)
+int main(int argc, char *argv[])
 {
 	int (*hi)(int, int);
+	int res;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
 	hi = get_op_func(argv[2]);
+
 	if(!hi)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", hi(atoi(argv[1], atoi(argv[3])));
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	res = hi(atoi(argv[1], atoi(argv[3])));
+	printf("%d\n", res);
 	return (0);
 }
