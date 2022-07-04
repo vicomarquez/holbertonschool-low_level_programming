feabe7081c0d316dad2fe9cdb2ec5977b1418658
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-03 18:03:09 -0700

    task 2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @n: numbersof strings
+ * @separator: string between numbers
+ *
+ *Return: 0
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arg;
+	unsigned int j;
+
+	va_start(arg, n);
+	for (j = 0 ; j < n ; j++)
+	{
+		if (separator && j < n - 1)
+			printf("%s", separator);
+		else if (va_arg(arg, char *) == NULL)
+			printf("(nil)");
+		else 
+			printf("%s", separator);
+
+	}
+	va_end(arg);
+	printf("\n");
+}
