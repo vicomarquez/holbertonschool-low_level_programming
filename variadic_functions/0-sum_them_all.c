bb1215ba67e999e500024595fe379d93d7d7dc83
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-03 17:09:44 -0700

    task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all parameters
+ *
+ *@n - unsigned int
+ *
+ *Return: sum
+ *
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list num;
+	int sum = 0;
+	int j;
+
+	if (n == 0)
+		return (0);
+
+	va_start (num, n);
+
+	for (j = 0 ; j < n ; j++)
+		sum += va_arg (num, int);
+
+	va_end (num);
+	return (sum);
+}
