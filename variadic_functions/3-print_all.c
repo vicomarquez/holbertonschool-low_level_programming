edaec5df6bf222181eb6f0fdcf3cb3f746889e25
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-04 19:19:38 -0700

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -15,36 +15,36 @@ void print_all(const char * const format, ...)
 	va_list arg;
 	unsigned int j = 0;
 
-	while (format != 0)
+	va_start(arg, format);
+	while (format && format[j] != '\0')
 	{
-		va_start(arg, format);
-		while (format[j])
+		switch (format[j])
 		{
-			switch (format[j])
-			{
-				case 0:
-					printf("%c", va_arg(arg, int));
+			case 0:
+				printf("%c", va_arg(arg, int));
+				break;
+			case 1:
+				printf("%d", va_arg(arg, int));
+				break;
+			case 2:
+				printf("%f", va_arg(arg, double));
+				break;
+			case 3:
+				if (format[j] == '\0')
+				{
+					printf("(nil)");
 					break;
-				case 1:
-					printf("%d", va_arg(arg, int));
-					break;
-				case 2:
-					printf("%f", va_arg(arg, double));
-					break;
-				case 3:
-					if (format[j] == '\0')
-					{
-						printf("(nil)");
-						break;
-					}	
-					printf("%s", va_arg(arg, char *));
-					break;
-			}
-			if (format[j + 1] != '\0' && format)
-				printf(", ");
-			j++;
+				}	
+				printf("%s", va_arg(arg, char *));
+				break;
+			default:
+				j++;
+				continue;
 		}
-		va_end(arg);
-		printf("\n");
+		if (format[j + 1] != '\0' && format)
+			printf(", ");
+		j++;
 	}
+	va_end(arg);
+	printf("\n");
 }
