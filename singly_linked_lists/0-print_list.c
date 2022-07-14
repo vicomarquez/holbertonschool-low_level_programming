04c14fdf20bc0f1058b95dd73b9d75134fb5d23e
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-13 21:37:23 -0700

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,8 +12,11 @@ size_t print_list(const list_t *h)
 {
 	size_t node = 0;
 	
+	if (h == NULL)
+		return (0);
+
 	if (h->str == NULL)
-		printf("[0] (nil)");
+		printf("[0] (nil)\n");
 	else
 		printf("[%d] %s\n", h->len, h->str);
 	node += 1;
