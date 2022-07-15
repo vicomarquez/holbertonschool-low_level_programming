f5ea962df7c3a13282215dad5e35d9b0f76d52f7
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-15 16:37:56 -0700

    Fixing betty´s whitesapce

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -24,10 +24,12 @@ list_t *add_node(list_t **head, const char *str)
 		;
 
 	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
 	new->str = strdup(str);
 	new->len = i;
 	new->next = *head;
 	*head = new;
-	
+
 	return (new);
 }
