c223adec30fa1b024cc75844727d681e47e554c8
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-15 19:15:57 -0700

    task 5

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * free_listint2 - frees a linked list
+ *
+ * @head: pointer to head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = *head;
+		while ((new = temp) != NULL)
+		{
+			temp = temp->next;
+			free(new);
+		}
+		*head = NULL;
+	}
+}
