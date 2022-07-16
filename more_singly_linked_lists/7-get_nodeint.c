96fbe5e77403ac1875ab52ae30254613eea00d04
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-15 19:43:53 -0700

    task 5

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,9 +14,10 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *temp
+	listint_t *temp;
 
 	temp = head;
+
 	while (i < index && temp != NULL)
 	{
 		temp = temp->next;
