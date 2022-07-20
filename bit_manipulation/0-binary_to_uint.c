5276c388bf6c423842e0faaba7542ad0b2e4abd0
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-19 18:46:43 -0700

    Fix cases

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -17,12 +17,12 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (b[i] != '\0')
+	for (; b[length] != '\0';)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[length] != '0' && b[length] != '1')
 			return (0);
 		else
-			i++;
+			length++;
 	}
 	while (i < length)
 	{
