e63582246c3a884d34074577c8c38acf929d03f4
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-20 06:37:30 -0700

    task 1

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define BIT_MANI
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+int _putchar(char);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
