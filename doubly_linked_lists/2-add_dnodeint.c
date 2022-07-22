266b7353ab525c8e9426901ed7af256e65f095b7
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-22 07:24:17 -0700

    Fix number of elements

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -23,6 +23,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->next = *head;
+	new->prev = NULL;
 	*head = new;
+	if (!*head)
+		(*head)->prev = new;
 	return (new);
 }
