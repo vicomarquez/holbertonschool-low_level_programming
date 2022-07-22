65cd472e2c6888536c8a0530e4c56fa516142d12
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-22 13:03:43 -0700

    task 5

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -34,7 +34,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	while (temp->next != NULL)
 		temp = temp->next;
-	temp->next = next;
+	temp->next = new;
 	new->prev = temp;
 
 	return (*head);
--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-	listint_t *temp;
+	dlistint_t *temp;
 
 	temp = head;
 
