b3a843b7ead8388a5c7e3c1baf206e220a8d2749
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-25 18:09:41 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -25,9 +25,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 			free(sasha->owner);
 			free(sasha);
 		}
-		else
-			free(sasha);
-		return (sasha);
+	}
+	else
+		free(sasha);
+	return (sasha);
 }
 
 /**
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,7 +20,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
 void free_dog(dog_t *d);
-int _strlen(char *a);
-char *_strdup(char *str);
+int _strlen(char *);
+char *_strdup(char *);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
