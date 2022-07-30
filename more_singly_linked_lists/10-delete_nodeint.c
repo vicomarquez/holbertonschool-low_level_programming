2390aefa2eb2159885a8be6c046a2e434dbe4f17
vicomarquez <vicomarquezmuniz@gmail.com>
2022-07-30 05:51:19 -0700

    fix cases

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (; i < index - 1 ; j++)
+	for (; j < index - 1 ; j++)
 	{
 		if (new->next == NULL)
 			return (-1);
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	delete = new->next;
-	new->next = next->next;
+	new->next = delete->next;
 
 	free(delete);
 
