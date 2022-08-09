d6116983a2fdc753c67238b8068dea848437e797
vicomarquez <vicomarquezmuniz@gmail.com>
2022-08-09 16:15:49 -0700

    task 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverses content of an array of integers
+ * @a: character to check
+ * @n: character to check
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j;
+
+	while (i < n - 1)
+	{
+		j = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = j;
+		n--;
+		i++;
+	}
+	
+}
