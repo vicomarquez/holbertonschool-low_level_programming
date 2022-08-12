63e5e6ec3733ca0586893c8c67bb0a1a818b2525
vicomarquez <vicomarquezmuniz@gmail.com>
2022-08-12 11:25:55 -0700

    task 0

--- file_io/main.h
+++ file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE
+#define FILE
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,3 +10,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+#endif
