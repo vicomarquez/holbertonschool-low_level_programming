13664364b3e51ea2e7a1c3c33e5eed960d4d6b61
vicomarquez <vicomarquezmuniz@gmail.com>
2022-08-30 10:58:40 -0700

    fix find different values x2

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -18,10 +18,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
-			printf("Found %ld at index: %d\n", i, array[i]);
 			return (i);
-		}
 	}
 	return (-1);
 }
