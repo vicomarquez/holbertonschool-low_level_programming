b2db62551f175fa362ca8b305d6e77eaf5a43fe1
vicomarquez <vicomarquezmuniz@gmail.com>
2022-08-30 10:43:11 -0700

    task 0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: pointer to first element
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: first index where value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!value || array == NULL)
+		return (-1);
+
+	for (i = 0 ; i < size ; i++)
+	{
+		printf ("Value checked array [%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Found %ld at index: %d\n", i, array[i]);
+			break;
+		}
+	}
+	return (0);
+}
